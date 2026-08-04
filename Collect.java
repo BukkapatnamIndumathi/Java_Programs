3a4ad7ce1c16a4eafd9aa33ad40e00b2bb9e1fa8
BukkapatnamIndumathi <bukkapatnamindumathi@gmail.com>
2026-08-04 23:22:11 +0530

    Add files via upload

---
+++ Collect.java
@@ -0,0 +1,26 @@
+package Arrays;
+import java.util.HashMap;
+
+public class Collect {
+
+	public static void main(String[] args) {
+	     int[] arr= {1,2,4,1,5,6,2};
+		
+		HashMap<Integer,Integer> map=new HashMap<>();
+		
+		
+		for(int num:arr) {
+			if(map.containsKey(num)) {
+				map.put(num,map.get(num)+1);
+			}
+			
+			else {
+				map.put(num,1);
+			}
+		}
+		System.out.println(map);
+		System.out.println(map.containsKey(2));
+
+	}
+
+}
